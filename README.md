1b55f3ada9ff2ea354c8d5b714683c70a4e09a3e
HristoTaushanov <123230359+HristoTaushanov@users.noreply.github.com>
2024-07-07 11:46:29 +0300

    Copy projects from Spring Initializer

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # FOLDING BOATS
 ## Intro
 This is a Spring Boot Java project.
-I've designed and created three types of folding boast with a lot of passion. 
+I've designed and created three types of folding boats with a lot of passion. 
 Currently, they are stored in my garage and I need to find out your opinion about them.
 
 ## Domain model
---
+++ src/main/java/bg/softuni/emptyproject/EmptyProjectApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.emptyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmptyProjectApplication.class, args);
+    }
+
+}
