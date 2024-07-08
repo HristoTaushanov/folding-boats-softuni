dddb5bfbb9e1dd94fc04b0562f0d359c0ae67a33
HristoTaushanov <123230359+HristoTaushanov@users.noreply.github.com>
2024-07-08 16:59:36 +0300

    Added index.html file.

---
+++ src/test/java/bg/softuni/emptyproject/EmptyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.emptyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
