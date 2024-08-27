070c3f229a01d244d0fac0dfbfc9ebc254a38277
HristoTaushanov <123230359+HristoTaushanov@users.noreply.github.com>
2024-08-27 11:15:01 +0300

    Updated the UniqueUserEmailValidator class.

--- src/main/java/bg/softuni/boats/validation/UniqueUserEmailValidator.java
+++ src/main/java/bg/softuni/boats/validation/UniqueUserEmailValidator.java
@@ -1,10 +1,12 @@
 package bg.softuni.boats.validation;
 
 
-import bg.softuni.boats.model.entity.UserEntity;
 import bg.softuni.boats.service.UserService;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 
 import java.util.Optional;
 
@@ -17,6 +19,30 @@ public class UniqueUserEmailValidator implements ConstraintValidator<UniqueUserE
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getPrincipal().equals("anonymousUser")) {
+            return userService
+                    .findUserByEmail(value)
+                    .isEmpty();
+        }
+
+        User principalUser = (User) authentication.getPrincipal();
+        Object principal = authentication.getPrincipal();
+
+        String principalUsername = principalUser.getUsername();
+        String principalEmail = userService.findUserByUsername(principalUsername).getEmail();
+
+        if (!principal.equals("anonymousUser") && value.equals(principalEmail)) {
+            return true;
+        }
+
+        if (!principal.equals("anonymousUser") && userService.findUserByEmail(value).isEmpty()) {
+            return true;
+        } else if (userService.findUserByEmail(value).isPresent()){
+            return false;
+        }
+
         return userService
                 .findUserByEmail(value)
                 .isEmpty();
